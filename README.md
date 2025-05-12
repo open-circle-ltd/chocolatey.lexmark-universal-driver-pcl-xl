605ec35811298d6cc369fcc5a08e3ab33b1eeda7
kve-occ <210706414+kve-occ@users.noreply.github.com>
2025-05-12 15:43:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Chocolatey Package: Lexmark Universal Printdriver PCL XL
+
+## Description
+
+This Package will install the Lexmark Universal Printdriver PCL XL v3.0.7. 
+
+## Installation
+
+```ps1
+choco install lexmark-universal-driver-pcl-xl
+```
+
+## TODO
+
+- Integrate ChocoMilk Updater
+    Lexmark does not provide a centralized directory or listing for available versions. May not be feaseable
+
+## Disclaimer
+
+These Chocolatey Packages only contain installation routines. The software itself is downloaded from the official sources of the software developer. Open Circle AG has no affilation with the software developer.
+
+## Author
+
+- [kve](https://github.com/kve-occ)
+- [Open Circle AG](https://www.open-circle.ch)
---
+++ package/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://downloads.lexmark.com/downloads/drivers/Lexmark_Universal_v2_UD1_Installation_Package_03072025.exe'
+$checksum = '29282342698A28EF4081F9432E19D2BB843EAF0A5488CCBB5459F08D021247048FF003AA862AC4314739CF7290CCFDAE62FE89A77DD9BD2B39C53BE597C0AB34'
+
+$tempDir = Join-Path $env:TEMP $packageName
+$exePath = Join-Path $tempDir "$packageName.exe"
+$7zPath = Join-Path $env:ProgramFiles "7-Zip\7z.exe"
+$7zArguments = "x `"$exePath`" -o`"$tempDir\extract`" -y"
+$msiPath = Join-Path $tempDir "\extract\InstallationPackage\Drivers\x64\print64XL.msi"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = $msiPath
+  checksum      = $checksum
+  checksumType  = 'sha512'
+  destination   = $toolsDir
+
+  softwareName  = 'Lexmark Universal v2 XL Print Driver'
+  fileType      = 'msi'
+  silentArgs    = '/quiet /norestart'
+}
+
+# Create Temp Directory
+New-Item -ItemType Directory -Force -Path $tempDir
+New-Item -ItemType Directory -Force -Path $tempDir\extract
+
+# Download/Extract .exe
+Invoke-WebRequest -Uri $url -OutFile $exePath
+Start-Process -FilePath $7zPath -ArgumentList $7zArguments -Wait -NoNewWindow
+
+Install-ChocolateyPackage @packageArgs
