197b3d494d8371bd27fa3c48a45fdce5a5e90e5a
kve-occ <210706414+kve-occ@users.noreply.github.com>
2025-05-20 09:43:04 +0200

    Remove comments

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -2,24 +2,22 @@ $ErrorActionPreference = 'Stop';
 $PackageParameters = Get-PackageParameters
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = $env:ChocolateyPackageName
+
 $url = 'https://downloads.lexmark.com/downloads/drivers/Lexmark_Universal_v2_UD1_Installation_Package_03072025.exe'
 $checksum = '017ae8977f2714409c25e8a54da69e94f027f32712196a0334d68ad1277080217ff5a76fef105d31189003a580f86335db8ef1d8bf3e045539b2e028771a5836'
 
 $tempDir = Join-Path $env:TEMP $packageName
 $exePath = Join-Path $tempDir "$packageName.exe"
+$logPath = Join-Path $tempDir "install.log"
 $7zPath = Join-Path $env:ProgramFiles "7-Zip\7z.exe"
 $7zArguments = "x `"$exePath`" -o`"$tempDir\extract`" -y"
 
-# Available drivers
-$driverTypes = @("PCL", "XL", "PostScript_Emulation")
-
-# Set defaults
 $arch = "64"
 $driverType = "XL"
 
 if ($PackageParameters) {
 
-    # /32bit parameter check
     if ($PackageParameters["32bit"]) {
       Write-Host "Installing 32-bit version."
       $arch = "86"
@@ -27,7 +25,6 @@ if ($PackageParameters) {
       Write-Host "Installing 64-bit version."
     }
 
-    # /Product parameter check
     if ($PackageParameters["Product"]) {
         $product = $PackageParameters["Product"]
         switch ($product) {
@@ -44,7 +41,6 @@ if ($PackageParameters) {
     Write-Host "Installing $driverType Driver."
 }
 
-# Construct path to .msi
 $driverFile = switch ($driverType) {
     "PCL" { "print$arch`PCL.msi" }
     "XL" { "print$arch`XL.msi" }
@@ -52,18 +48,12 @@ $driverFile = switch ($driverType) {
   }
 $msiPath = Join-Path $tempDir "\extract\InstallationPackage\Drivers\x$arch\$driverFile"
 
+New-Item -ItemType Directory -Force -Path $tempDir | Out-Null
+New-Item -ItemType Directory -Force -Path $tempDir\extract | Out-Null
 
-# Create Temp Directory
-# Silence Output
-New-Item -ItemType Directory -Force -Path $tempDir
-New-Item -ItemType Directory -Force -Path $tempDir\extract
-
-# Download/Extract .exe
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $exePath -Url $url -Checksum $checksum -ChecksumType 'sha512'
+Start-Process -FilePath $7zPath -ArgumentList $7zArguments -Wait -NoNewWindow -RedirectStandardOutput $logPath -RedirectStandardError $logPath
 
-Start-Process -FilePath $7zPath -ArgumentList $7zArguments -Wait -NoNewWindow
-
-# Install package
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'msi'
